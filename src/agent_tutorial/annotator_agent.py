6d12915c087431ed656b4b6133b394a4f082b3d1
dosumis <dosumis@gmail.com>
2025-07-03 14:31:34 +0100

    Some hacking to turn this into a CL annotating agent.
    Query hacked in to code for now.
    However, it seems to work pretty well.

--- src/agent_tutorial/annotator_agent.py
+++ src/agent_tutorial/annotator_agent.py
@@ -17,23 +17,28 @@ class TextAnnotation(BaseModel):
 class TextAnnotationResult(BaseModel):
     annotations: List[TextAnnotation]
 
-annotator_agent = Agent(  
+annotator_agent = Agent(
     #'claude-3-7-sonnet-latest',
     'openai:gpt-4o',
     system_prompt="""
     You will be provided with a table where each row represents a cell type.
-    Your goal is to map each row to a term from the cell ontology. The first columns is a CL labelExtract all cl terms from the text. Return the as a list of annotations.
-    Be sure to include all spans mentioning cell types; if you cannot
-    find a cl ID, then you should still return a TextAnnotation, just leave
+    Your goal is to map text in each row to terms from the cell ontology.
+    Return the as a list of annotations.
+    Be sure to include all spans mentioning cell types;
+    convert all plurals to singular before searching;
+    if you cannot find a cl ID, then you should still return a TextAnnotation, just leave
     the cl_id field empty.
 
     However, before giving up you should be sure to try different combinations of
-    synonyms with the `search_cl` tool.
+    synonyms with the `search_cl` tool. When you try searching synonyms, please also
+    try substituting some terms in the span with common synonyms of those terms.  
+    Also try converting between the forms 'X Y' and 'Y of X' where X is a tissue 
+    or anatomical structure and Y is a cell type.
     """,
     tools=[search_cl],
     result_type=TextAnnotationResult,  
 )
-DEFAULT_TEXT = 'The (hippocampal) CA1 is located in the forebrain, it projects to the amygdala, subiculum, and the entorhinal cortex'
+DEFAULT_TEXT = 'C_goblet	Colon goblet cells	Colonic goblet cells; Mucus-producing cells; GC	Crypt-resident goblet cells; Intercrypt goblet cells; Early goblet cells'
 
 @click.command()
 @click.argument('text', default=DEFAULT_TEXT)
